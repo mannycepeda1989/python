17598d42dd9a6bd5bd7eef926cee156eff0cacfd
mannycepeda1989 <100322499+mannycepeda1989@users.noreply.github.com>
2024-07-09 16:33:34 -0700

    Create wagecalculator.py

---
+++ wagecalculator.py
@@ -0,0 +1,11 @@
+def computepay(hrs, rate):
+    if hrs > 40:
+        return (40*rate)+(hrs-40)*(rate*1.5)
+    else:
+        return hrs*rate
+hrs = input('Please enter your hours: ')
+hrs = float(hrs)
+rate = input ('Please enter your rate: ')
+rate = float(rate)
+p = computepay(hrs,rate)
+print("Pay", p)
